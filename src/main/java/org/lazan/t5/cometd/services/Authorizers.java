c48d2ca2c18a6136400ae2e3e1e14149c9237252
Lance <uklance@gmail.com>
2012-06-22 10:07:56 +0200

    Update master

--- src/main/java/org/lazan/t5/cometd/services/Authorizers.java
+++ src/main/java/org/lazan/t5/cometd/services/Authorizers.java
@@ -2,6 +2,7 @@ package org.lazan.t5.cometd.services;
 
 import org.cometd.bayeux.server.Authorizer;
 
-public interface Authorizers extends Authorizer {
 
+public interface Authorizers extends Authorizer {
+   // TODO: support thread-safe addAuthorizer() and removeAuthorizer()
 }
