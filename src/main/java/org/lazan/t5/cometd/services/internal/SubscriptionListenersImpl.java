c7a7ff8ad88d67fa7e24136de7c83e2801608888
Lance Java <uklance@gmail.com>
2012-06-23 20:44:05 +0100

    Revert "Revert "Use ordered config for authorizers and subscription listeners""
    This reverts commit 3d246aa72758a4fc6cf2cbb1d6d6e85ce4e30bc9.

--- src/main/java/org/lazan/t5/cometd/services/internal/SubscriptionListenersImpl.java
+++ src/main/java/org/lazan/t5/cometd/services/internal/SubscriptionListenersImpl.java
@@ -1,18 +1,18 @@
 package org.lazan.t5.cometd.services.internal;
 
-import java.util.Collection;
+import java.util.List;
 
-import org.apache.tapestry5.ioc.annotations.UsesConfiguration;
+import org.apache.tapestry5.ioc.annotations.UsesOrderedConfiguration;
 import org.lazan.t5.cometd.ClientContext;
 import org.lazan.t5.cometd.TopicMatchers;
 import org.lazan.t5.cometd.services.SubscriptionListener;
 import org.lazan.t5.cometd.services.SubscriptionListeners;
 
-@UsesConfiguration(SubscriptionListener.class)
+@UsesOrderedConfiguration(SubscriptionListener.class)
 public class SubscriptionListenersImpl implements SubscriptionListeners {
 	private final TopicMatchers<SubscriptionListener> listeners;
 	
-	public SubscriptionListenersImpl(Collection<SubscriptionListener> list) {
+	public SubscriptionListenersImpl(List<SubscriptionListener> list) {
 		listeners = new TopicMatchers<SubscriptionListener>();
 		for (SubscriptionListener listener : list) {
 			listeners.add(listener.getTopicPattern(), listener);
