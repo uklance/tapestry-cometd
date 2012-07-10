8f33342c4cd69ab5386a86188a88bc16e3fe422f
Lance Java <uklance@gmail.com>
2012-07-10 18:35:50 +0100

    Cleaning up interfaces

--- src/main/java/org/lazan/t5/cometd/services/Authorizers.java
+++ src/main/java/org/lazan/t5/cometd/services/Authorizers.java
@@ -1,9 +1,6 @@
 package org.lazan.t5.cometd.services;
 
-import org.cometd.bayeux.server.Authorizer;
-
-
-public interface Authorizers extends Authorizer {
-	public void addAuthorizer(org.lazan.t5.cometd.services.Authorizer auth);
-	public boolean removeAuthorizer(org.lazan.t5.cometd.services.Authorizer auth);
+public interface Authorizers extends org.cometd.bayeux.server.Authorizer {
+	public void addAuthorizer(Authorizer auth);
+	public boolean removeAuthorizer(Authorizer auth);
 }
--- src/main/java/org/lazan/t5/cometd/services/SubscriptionListeners.java
+++ src/main/java/org/lazan/t5/cometd/services/SubscriptionListeners.java
@@ -1,8 +1,6 @@
 package org.lazan.t5.cometd.services;
 
-import org.cometd.bayeux.server.BayeuxServer.SubscriptionListener;
-
-public interface SubscriptionListeners extends SubscriptionListener {
-	public void addListener(org.lazan.t5.cometd.services.SubscriptionListener listener);
-	public boolean removeListener(org.lazan.t5.cometd.services.SubscriptionListener listener);
+public interface SubscriptionListeners extends org.cometd.bayeux.server.BayeuxServer.SubscriptionListener {
+	public void addListener(SubscriptionListener listener);
+	public boolean removeListener(SubscriptionListener listener);
 }
