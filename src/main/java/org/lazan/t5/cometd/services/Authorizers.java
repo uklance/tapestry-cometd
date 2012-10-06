2f1e74282b188f6b82545c4463e23d4ede5bf4e8
uklance <uklance@gmail.com>
2012-10-06 15:46:41 +0100

    Adding a bit of javadoc

--- src/main/java/org/lazan/t5/cometd/services/Authorizers.java
+++ src/main/java/org/lazan/t5/cometd/services/Authorizers.java
@@ -2,18 +2,29 @@ package org.lazan.t5.cometd.services;
 
 public interface Authorizers extends org.cometd.bayeux.server.Authorizer {
 	/**
-	 * @param topic	A topic which may end in a wildcard (either "*" to match a single level or "**" to match all levels
+	 * Map an authorizer to a topic pattern. The authorizer will be fired for any subscriptions 
+	 * attempt to a topic that matches the provided topic pattern
+	 * 
+	 * @param topic	A topic which may end in a wildcard (either "*" to match a single level or "**" to match all levels)
 	 * 					/chat/cars/porsche will match /chat/cars/porsche
 	 *					/chat/* will match /chat/cars and /chat/java
 	 *					/chat/** will match /chat/cars and /chat/cars/porsche
+	 *
+	 * @param auth An authorizer
 	 */
 	public void addAuthorizer(String topic, Authorizer auth);
 
 	/**
-	 * @param topic	A topic which may end in a wildcard (either "*" to match a single level or "**" to match all levels
+	 * Remove the authorizer mapped to a topic
+	 * 
+	 * @param topic	A topic which may end in a wildcard (either "*" to match a single level or "**" to match all levels)
 	 * 					/chat/cars/porsche will match /chat/cars/porsche
 	 *					/chat/* will match /chat/cars and /chat/java
 	 *					/chat/** will match /chat/cars and /chat/cars/porsche
+	 *
+	 * @param auth The authorizer to remove
+	 * 
+	 * @return true if the authorizer was removed, false otherwise
 	 */
 	public boolean removeAuthorizer(String topic, Authorizer auth);
 }
--- src/main/java/org/lazan/t5/cometd/services/SubscriptionListeners.java
+++ src/main/java/org/lazan/t5/cometd/services/SubscriptionListeners.java
@@ -2,7 +2,10 @@ package org.lazan.t5.cometd.services;
 
 public interface SubscriptionListeners extends org.cometd.bayeux.server.BayeuxServer.SubscriptionListener {
 	/**
-	 * @param topic	A topic which may end in a wildcard (either "*" to match a single level or "**" to match all levels
+	 * Map a subscription listener to a topic pattern. The listener will be fired for all messages 
+	 * sent to a topic that matches the provided topic pattern
+	 * 
+	 * @param topic	A topic which may end in a wildcard (either "*" to match a single level or "**" to match all levels)
 	 * 					/chat/cars/porsche will match /chat/cars/porsche
 	 *					/chat/* will match /chat/cars and /chat/java
 	 *					/chat/** will match /chat/cars and /chat/cars/porsche
@@ -10,10 +13,14 @@ public interface SubscriptionListeners extends org.cometd.bayeux.server.BayeuxSe
 	public void addListener(String topic, SubscriptionListener listener);
 
 	/**
-	 * @param topic	A topic which may end in a wildcard (either "*" to match a single level or "**" to match all levels
+	 * Remove the subscription listener mapped to a topic
+	 * 
+	 * @param topic	A topic which may end in a wildcard (either "*" to match a single level or "**" to match all levels)
 	 * 					/chat/cars/porsche will match /chat/cars/porsche
 	 *					/chat/* will match /chat/cars and /chat/java
 	 *					/chat/** will match /chat/cars and /chat/cars/porsche
+	 *
+	 * @return true if the authorizer was removed, false otherwise
 	 */
 	public boolean removeListener(String topic, SubscriptionListener listener);
 }
